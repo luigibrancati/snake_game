973496626a05c6c5a675e4c94a236982cb2feba5
Luigi Brancati <luigi.brancati93@gmail.com>
2021-06-17 14:24:38 +0200

    Fixed window position, first implementation of arrow keys

--- snake.cpp
+++ snake.cpp
@@ -9,12 +9,16 @@
 using std::cout; using std::cin; using std::endl;
 
 const int base_speed = 50000;
+int x0_val, y0_val;
+
+
 
 void printBoard(WINDOW*, const Board&, const Food&, const Snake&, int);	
 bool checkLose(const Board&, const Snake&);
 short convertMove(WINDOW*);
 static void fine(int, int);
 void runGame();
+int set_speed_input(int);
 
 int main(){
 	std::srand(std::time(NULL));
@@ -26,37 +30,44 @@ int main(){
 	(void) cbreak();       /* prende i caratteri in input uno alla volta, senza attendere il \n */ 
 	(void) noecho();       /* nessuna echo dell'input */ 	
 	
+
+	getmaxyx(stdscr, y0_val,x0_val);
+	cout<<y0_val<<x0_val<<endl;
 	runGame();
 	return 0;
 }
 
 void runGame(){
 	WINDOW * win = newwin(2,50,0,0);
-	int d, move, speed_val=1, score = 0;
-	char str[3], speed[3];
+	int board_d, w_height, w_width, move, speed_val=1, score = 0;
+	char str[5], speed[5];
+	bool gameState = true;
 	
 	waddstr(win, "WELCOME TO SHITTY SNAKE!\n");
 	waddstr(win, "Choose the dimension of the board: ");
 	wgetstr(win, str);
-	d = std::atoi(str);
+	board_d = std::atoi(str);
 	wclear(win);
 	wrefresh(win);
+
 	waddstr(win, "Choose the speed (1=fastest, 2=slower, etc.): ");
 	wgetstr(win, speed);
 	speed_val = std::atoi(speed);
 	wclear(win);
 	wrefresh(win);
 
-	win = newwin(d+3,2*d,0,0);
-	Board board(d);
-	Food food(std::rand()%d,std::rand()%d);
-	Snake snake(d/2, d/2);
-	bool gameState = true;
+	w_height = board_d+3; //3 rows more to print Score and loosing message
+	w_width = 2*board_d; //twice because i print ". " as a board cell	
+	win = newwin(w_height,w_width,(y0_val-w_height)/2,(x0_val-w_width)/2);
+	Board board(board_d);
+	Food food(std::rand()%board_d,std::rand()%board_d);
+	Snake snake(board_d/2, board_d/2);
 	
+	//first print	
 	printBoard(win,board,food,snake,score);
 	wrefresh(win);
 	snake.move(convertMove(win));
-	nodelay(win, true);
+	nodelay(win, true);//don't wait for input after this
 
 	while(gameState){
 		snake.move(convertMove(win));
@@ -65,18 +76,22 @@ void runGame(){
 		gameState = !checkLose(board,snake);
 		if(food.getX() == snake.getX()[0] && food.getY() == snake.getY()[0]){
 			snake.eat();
-			food.replace(std::rand()%(d-2)+1,std::rand()%(d-2)+1);
+			food.replace(std::rand()%board_d,std::rand()%board_d);
 			score++;
 		}
-		usleep(speed_val*base_speed);
+		usleep(set_speed_input(speed_val));
 	}
 
-	nodelay(win, false);
+	nodelay(win, false); //wait input
 	printBoard(win,board,food,snake,score);
 	waddstr(win, "You lost!");
 	wrefresh(win);
 	wgetch(win);
 	fine(0, score);
+}
+
+int set_speed_input(int speed_val){
+	return (speed_val==KEY_ENTER?base_speed:speed_val*base_speed);
 }
 
 void printBoard(WINDOW * win, const Board & board, const Food & food, const Snake & snake, int score){
@@ -116,10 +131,10 @@ bool checkLose(const Board& board, const Snake& snake){
 
 short convertMove(WINDOW * win){
 	int m = wgetch(win);
-	if(m==((int)'w')) return 0;
-	else if(m==((int)'a')) return 1;
-	else if(m==((int)'s')) return 2;
-	else if(m==((int)'d')) return 3;
+	if(m==((int)'w') || m==KEY_UP) return 0;
+	else if(m==((int)'a') || m==KEY_LEFT) return 1;
+	else if(m==((int)'s') || m==KEY_DOWN) return 2;
+	else if(m==((int)'d') || m==KEY_RIGHT) return 3;
 	return -1;
 }
 
