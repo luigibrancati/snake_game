838c6b6789e77ca50967efcb635f1757c77bbdea
Luigi Brancati <luigi.brancati93@gmail.com>
2021-06-23 20:44:46 +0200

    New end scree

--- snake.cpp
+++ snake.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <ncurses.h>
+#include <string>
 #include <unistd.h>
 #include "game.hpp"
 using std::cout; using std::cin; using std::endl;
@@ -9,9 +10,10 @@ using std::cout; using std::cin; using std::endl;
 const int base_speed = 50000;
 int x0_val = 0, y0_val = 0;
 
-static void fine(int, int);
-void runGame();
+void fine(WINDOW*, int, int);
+void runGame(WINDOW*);
 int set_speed_input(int);
+void wreset(WINDOW*);
 
 int main(){
 	std::srand(std::time(NULL));
@@ -27,33 +29,36 @@ int main(){
 	getmaxyx(stdscr, y0_val,x0_val);
 	cout<<y0_val<<x0_val<<endl;
 	//run game
-	runGame();
+	WINDOW * win = newwin(2, 50, 0, 0);
+	keypad(win, TRUE);  /* abilita la mappatura della tastiera */ 
+	runGame(win);
 	return 0;
 }
 
-void runGame(){
-	WINDOW * win = newwin(2,50,0,0);
+void runGame(WINDOW * win){
 	int board_d, w_height, w_width, speed_val=1;
 	char str[5], speed[5];
-	
+
+	wreset(win);
+	mvwin(win, 0, 0);
+	wresize(win, 2, 50);
+	(void) echo(); //add input echo	
 	waddstr(win, "WELCOME TO SHITTY SNAKE!\n");
 	waddstr(win, "Choose the dimension of the board: ");
 	wgetstr(win, str);
 	board_d = std::atoi(str);
 	board_d = (board_d<15? 15:board_d);
-	wclear(win);
-	wrefresh(win);
-
+	wreset(win);
 	waddstr(win, "Choose the speed (1=fastest, 2=slower, etc.): ");
 	wgetstr(win, speed);
 	speed_val = std::atoi(speed);
-	wclear(win);
-	wrefresh(win);
+	wreset(win);
 
 	w_height = board_d+3; //3 rows more to print Score and losing message
-	w_width = 2*board_d; //twice because I print ". " as a board cell	
-	win = newwin(w_height,w_width,(y0_val-w_height)/2,(x0_val-w_width)/2);
-	keypad(win, TRUE);  /* abilita la mappatura della tastiera */ 
+	w_width = 2*board_d; //twice because I print ". " as a board cell
+	mvwin(win,(y0_val-w_height)/2,(x0_val-w_width)/2);
+	wresize(win, w_height, w_width);
+	wreset(win);
 
 	SnakeGame game(board_d);
 	
@@ -71,20 +76,31 @@ void runGame(){
 		usleep(set_speed_input(speed_val));
 	}
 	
-	nodelay(win, false); //wait input
 	usleep(base_speed);
-	game.printBoard(win);
-	waddstr(win, "You lost!");
-	wrefresh(win);
-	wgetch(win);
-	fine(0, game.getScore());
+	fine(win, 0, game.getScore());
 }
 
 int set_speed_input(int speed_val){
 	return (speed_val==KEY_ENTER?base_speed:speed_val*base_speed);
 }
 
-static void fine(int sig, int score){
-	endwin();
-	cout<<"You lost! Final score: "<<score<<endl;
+void fine(WINDOW * win, int sig, int score){
+	char out;
+	std::string output = "You lost! Final Score: "+std::to_string(score)+"\n";
+
+	nodelay(win, false); //wait input
+	wreset(win);
+	waddstr(win, output.c_str());
+	waddstr(win, "[R] Retry [Q] Quit");
+	wrefresh(win);
+	out = wgetch(win);
+	if(out == 'R' || out == 'r') runGame(win);
+	else if (out == 'Q' || out=='q') endwin();
+	else fine(win, sig, score);
+}
+
+void wreset(WINDOW * win){
+	wclear(win);
+	wmove(win, 0, 0);
+	wrefresh(win);
 }
